1a62f5f520894c392487bc46736ab04648c756e7
filip <filip.asp89@gmail.com>
2017-05-09 15:51:15 +0200

    removed the animation on charts to make the apps more similar

--- val-home/src/app/linechart.ts
+++ val-home/src/app/linechart.ts
@@ -21,6 +21,9 @@ export class LineChart {
       this.lineChart = new Chart(this.lineCanvas.nativeElement, {
 
         type: 'line',
+        options: {
+          animation: false
+        },
         data: {
           labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"],
           datasets: [
